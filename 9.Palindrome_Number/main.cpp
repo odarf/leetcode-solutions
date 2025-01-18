3c17345e565f607332506f5ac5381d754b62612a
odarf madven <danil.kashenya@gmail.com>
2025-01-18 15:36:23 +0300

    clang format

--- 9.Palindrome_Number/main.cpp
+++ 9.Palindrome_Number/main.cpp
@@ -1,17 +1,19 @@
 bool isPalindrome(int x) {
-            if(x<0) { return false; }
-            
-            unsigned int reversed = 0;
-            unsigned int temp = x;
+  if (x < 0) {
+    return false;
+  }
 
-            while(temp != 0){
-                reversed = reversed * 10 + temp % 10;
-                temp = temp / 10;
-            }
-            return reversed == x;
-        }
+  unsigned int reversed = 0;
+  unsigned int temp = x;
 
-int main(int argc, char* argv[]){
-    isPalindrome(int(argv[1]));
-    return 0;
+  while (temp != 0) {
+    reversed = reversed * 10 + temp % 10;
+    temp = temp / 10;
+  }
+  return reversed == x;
+}
+
+int main(int argc, char *argv[]) {
+  isPalindrome(int(argv[1]));
+  return 0;
 }
