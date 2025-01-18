678153d95f70e0a135e96256a60380c5c039808f
odarf madven <danil.kashenya@gmail.com>
2025-01-18 15:27:30 +0300

    solution for palindrome number

---
+++ 9.Palindrome_Number/main.cpp
@@ -0,0 +1,17 @@
+bool isPalindrome(int x) {
+            if(x<0) { return false; }
+            
+            unsigned int reversed = 0;
+            unsigned int temp = x;
+
+            while(temp != 0){
+                reversed = reversed * 10 + temp % 10;
+                temp = temp / 10;
+            }
+            return reversed == x;
+        }
+
+int main(int argc, char* argv[]){
+    isPalindrome(int(argv[1]));
+    return 0;
+}
